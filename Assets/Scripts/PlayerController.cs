7bca049fe5481bc10995e5966ef6804a28d31dc0
Sang Truong <minhsangtruong1999@gmail.com>
2019-10-05 17:49:17 +0700

    Prototype implemented.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float horizontalMove = 0f;
+
+    bool isJumping = false;
+    bool isCrouching = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetFloat("speed_f", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            animator.SetBool("isJumping_b", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            isCrouching = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+            isCrouching = false;
+
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, isCrouching, isJumping);
+    }
+    public void OnLanding()
+    {
+        isJumping = false;
+        animator.SetBool("isJumping_b", false);
+    }
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching_b", isCrouching);
+    }
+}
